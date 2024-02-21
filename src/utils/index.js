4d48fb0a11dccb97c6bd55c9cad81012b8342094
TheLastpressedkey <89164291+TheLastpressedkey@users.noreply.github.com>
2024-02-21 19:06:19 +0100

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,7 +11,7 @@ var { taskbar, desktop, pinned, recent } = {
   ],
   desktop: (localStorage.getItem("desktop") &&
     JSON.parse(localStorage.getItem("desktop"))) || [
-    "Hvxor",
+    "Blue",
     "Unescape",
     "Recycle Bin",
     "File Explorer",
