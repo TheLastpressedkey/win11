23d10976abff6f6616438452b04138f0e31a8e46
TheLastpressedkey <89164291+TheLastpressedkey@users.noreply.github.com>
2024-02-24 01:28:51 +0100

    Update store.jsx

--- src/containers/applications/apps/store.jsx
+++ src/containers/applications/apps/store.jsx
@@ -233,12 +233,12 @@ const DownPage = ({ action, apps }) => {
         </div>
         <div className="absolute right-0 mr-4 text-sm">
           <a
-            href="https://win11react-docs.andrewstech.me/docs/Store/add-app"
+            href="#https://win11react-docs.andrewstech.me/docs/Store/add-app"
             className="catbtn"
             target="_blank"
             rel="noreferrer"
           >
-            Add your own app
+            Vous êtes developpeur ?
           </a>
         </div>
       </div>
